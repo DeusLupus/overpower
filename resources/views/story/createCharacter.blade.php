f727819e8cf193c2f1fbd48312a2639d836ef708
Deo Bissessar <Deo Bissessar>
2017-01-11 13:30:54 -0500

    Procfile for Heroku

--- resources/views/story/createCharacter.blade.php
+++ resources/views/story/createCharacter.blade.php
@@ -3,9 +3,42 @@
 @section('content')
 <div class="container">
 
-    <div class="panel-heading">
-        Create Your Character
+    <div>
+        <h3>Create Your Character</h3>
     </div>
+
+    <form>
+        <div class="form-group">
+            <label for="characterName">Character Name</label>
+            <input type="text" class="form-control" id="characterName" placeholder="Character Name">
+        </div>
+
+        <form>
+            <div class="row">
+                <div class="col-xs-4">
+                    <input type="radio" name="gender" value="male" checked> Male<br>
+                </div>
+                <div class="col-xs-4">
+                    <input type="radio" name="gender" value="female"> Female<br>
+                </div>
+                <div class="col-xs-4">
+                    <input type="radio" name="gender" value="other"> Other
+                </div>
+            </div>
+        </form>
+
+        <div class="row">
+            <div class="col-lg-6">
+                <button type="button" class="btn btn-default">Generate Stats</button>
+            </div>
+        </div>
+
+        <div class="row">
+            <div class="col-lg-6">
+                <button type="submit" class="btn btn-default">Submit</button>
+            </div>
+        </div>
+    </form>
 
 </div>
 
